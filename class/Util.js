f28c86aaebe4e35183c00b64dd8f2f27ee1394a2
louyongliang <lafeillou@126.com>
2018-01-08 21:00:06 +0800

    init repo

---
+++ class/Util.js
@@ -0,0 +1,66 @@
+const Util = (($) => {
+    let transition = false;
+
+    // 获取特殊过渡效果结束事件
+    function getSpecialTransitionEndEvent() {
+        return {
+            bindType: transition.end,
+            delegateType: transition.end,
+            handle(event) {
+                if ($(event.target).is(this)) {
+                    return event.
+                    handleObj.
+                    handler.
+                    apply(this, arguments);
+                }
+                return undefined;
+            }
+        }
+    }
+
+    // 过渡事件结束测试
+    function transitionEndTest() {
+        if (window.QUnit) {
+            return false;
+        }
+        return {
+            end: 'transitionend'
+        };
+    }
+    // 过渡结束枚举器
+    function transitionEndEmulator(duration) {
+        let called = false;
+        $(this).one(Util.TRANSITION_END, () => {
+            called = true;
+        });
+
+        setTimeout(() => {
+            if (!called) {
+                Util.triggerTransitionEnd(this);
+            }
+        }, duration);
+    }
+
+    // 设置过渡结束事件支持
+    function setTransitionEndSupport() {
+        transition = transitionEndTest();
+        $.fn.mmEmulateTransitionEnd = transitionEndEmulator;
+
+        if (Util.supportsTransitionEnd()) {
+            $.event.special[Util.TRANSITION_END] = getSpecialTransitionEndEvent();
+        }
+    }
+
+    const Util = {
+        TRANSITION_END: 'mmTransitionEnd',
+        triggerTransitionEnd(element) {
+            $(element).trigger(transition.end);
+        },
+        supportsTransitionEnd() {
+            return Boolean(transition);
+        }
+    }
+
+    setTransitionEndSupport();
+    return Util;
+})(jQuery);
